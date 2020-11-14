c7c88bdd8180ba365cf32733e9f8963f79519d87
Ronald Mnjoma <simbiso@gmail.com>
2020-11-14 09:10:58 +0200

    Update targets.js

--- ebs_with_tracing/targets.js
+++ ebs_with_tracing/targets.js
@@ -1,8 +1,6 @@
 const extras = require('./nools-extras');
 const {
   isAlive,
-  getSubsequentFollowUps,
-  isActiveCovid,
   getField
 } = extras;
 
@@ -15,7 +13,6 @@ const isToday = (someDate) => {
         someDate.getFullYear() === today.getFullYear();
 };
 
-const isRegistrationForm = (report) => { return report.form === 'form_a0'; };
 const isHbcFollowupForm = (report) => { return report.form === 'hbc_followup'; };
 
 module.exports = [
@@ -80,24 +77,6 @@ module.exports = [
         date: 'reported'
     },
 
-    {
-        id: 'travellers-with-declaration-this-month',
-        type: 'percent',
-        icon: 'icon-form-general',
-        goal: 100,
-        translation_key: 'targets.declaration.percent',
-        subtitle_translation_key: 'targets.this_month.subtitle',
-        percentage_count_translation_key: 'targets.traveller.percent',
-        context:'user.role === "inputter"',
-        appliesTo: 'contacts',
-        appliesToType: ['person'],
-        appliesIf: isPositive,
-        passesIf: function (contact) {
-            return contact.reports.some((report) => { return isDeclarationForm(report); });
-        },
-        date: 'reported'
-    },
-
 
     {
         id: 'travellers-with-quarantine-today',
@@ -116,56 +95,6 @@ module.exports = [
         date: 'reported'
     },
 
-    {
-        id: 'travellers-with-quarantine-this-month',
-        type: 'percent',
-        icon: 'icon-calendar',
-        goal: 100,
-        translation_key: 'targets.quarantine.percent',
-        subtitle_translation_key: 'targets.this_month.subtitle',
-        context:'user.role === "inputter"',
-        appliesTo: 'contacts',
-        appliesToType: ['person'],
-        appliesIf: isPositive,
-        passesIf: function (contact) {
-            return contact.reports.some((report) => { return isQuarantineForm(report); });
-        },
-        date: 'reported'
-    },
-
-    {
-        id: 'travellers-with-locator-this-month',
-        type: 'percent',
-        icon: 'icon-service-rating',
-        goal: 100,
-        translation_key: 'targets.locator.percent',
-        subtitle_translation_key: 'targets.this_month.subtitle',
-        context:'user.role === "inputter"',
-        appliesTo: 'contacts',
-        appliesToType: ['person'],
-        appliesIf: isPositive,
-        passesIf: function (contact) {
-            return contact.reports.some((report) => { return isLocatorForm(report); });
-        },
-        date: 'reported'
-    },
-
-    {
-        id: 'travellers-with-referral-today',
-        type: 'count',
-        icon: 'icon-hospital',
-        goal: 0,
-        translation_key: 'targets.referral.count',
-        subtitle_translation_key: 'targets.today.subtitle',
-        context:'user.role === "chw_supervisor"',
-        appliesTo: 'contacts',
-        appliesToType: ['person'],
-        appliesIf: function (contact) {
-            return isPositive(contact) &&
-                contact.reports.some((report) => { return isToday(new Date(report.reported_date)) && isReferralForm(report); });
-        },
-        date: 'reported'
-    },
 
     {
         id: 'patients-with-home-based-care-this-month',
